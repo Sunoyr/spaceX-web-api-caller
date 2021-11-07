b213cd0df05c9cf29a5f5f383d7eca3d9d2400d1
Thomas Legris <thomas@MacBook-Air-de-Thomas.local>
2021-11-07 23:12:14 +0100

    correction utilisation axios

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer>
+        <h3>Je suis un Footer de test</h3>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter'
+}
+</script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <header>
+        <h3>Je suis un header de test</h3>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
